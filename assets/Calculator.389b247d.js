15d23e99ac16685162492c49ae2dd6d74bd26319
xkre <xkre@users.noreply.github.com>
2022-10-06 09:34:46 +0000

    Deploying to gh-pages from @ xkre/malaysia-inflation-calc@474280685c8602c1fe7303ed4c14dbbe6a805b5e 🚀

---
+++ assets/Calculator.389b247d.js
@@ -0,0 +1,1 @@
+import"./index.066258ca.js";import{_ as r}from"./index.066258ca.js";export{r as default};
--- assets/Calculator.90754938.js
+++
@@ -1,1 +0,0 @@
-import"./index.de71f458.js";import{_ as r}from"./index.de71f458.js";export{r as default};
