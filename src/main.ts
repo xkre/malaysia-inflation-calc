72159c4a8fb36e7e430fa296c2230f02bab7976d
xkre <arekram@live.com>
2022-12-13 17:51:37 +0800

    Update service worker. Update tsconfig to support virtual: import

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import { useMatomoto } from './configuration/useMatamoto'
 import { router } from './router'
 import { setupWatch } from './state/routeState'
+import { registerSW } from 'virtual:pwa-register'
 
 import './assets/index.scss'
 
@@ -13,3 +14,8 @@ useMatomoto(app, router)
 app.mount('#app')
 
 setupWatch()
+
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
