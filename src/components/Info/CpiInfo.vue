5c756b20ba83d950be85edb8434f38dc75267f60
xkre <arekram@live.com>
2023-01-03 19:32:07 +0800

    Update description text for big mac index

--- src/components/Info/CpiInfo.vue
+++ src/components/Info/CpiInfo.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div v-if="cpiData.year1" class="py-1">
-      <span class="font-semibold">{{ year1 }}</span> CPI: {{ cpiData.year1 }}
+      <span class="font-semibold">{{ year1 }}</span> {{ description }}: {{ cpiData.year1 }}
     </div>
     <div v-else>
       No data
     </div>
     <div v-if="cpiData.year2" class="py-1">
-      <span class="font-semibold">{{ year2 }}</span> CPI: {{ cpiData.year2 }}
+      <span class="font-semibold">{{ year2 }}</span> {{ description }}: {{ cpiData.year2 }}
     </div>
     <div v-else>
       No data
@@ -19,6 +19,7 @@
 import { computed } from '@vue/reactivity'
 import { getCPI } from '@/services/inflationService'
 import { calculatorState } from '@/state/calculatorState'
+import { InflationType } from '@/types/InflatitonTypeEnum';
 
 const { year1, year2, inflationType, part } = calculatorState
 
@@ -26,5 +27,13 @@ const cpiData = computed(() => {
   const cpi = getCPI(year1.value, year2.value, inflationType.value, part.value)
 
   return cpi
+})
+
+const description = computed(() => {
+  if (inflationType.value === InflationType.BigMac) {
+    return 'Big Mac Price'
+  }
+
+  return 'CPI'
 })
 </script>
